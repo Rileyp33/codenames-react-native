9179b56ea3877a6ea90380b4d26c67ad2bd8355c
Riley Pratt <rpratt@adkadmins-MacBook-Pro.local>
2019-06-02 04:40:17 -0400

    API response

--- src/navigation/home/index.js
+++ src/navigation/home/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import {Text} from 'react-native'
+import axios from 'axios'
 
 export default class HomeScreen extends React.Component{
   constructor(props) {
     super(props)
     this.state = {}
+  }
+
+  componentDidMount() {
+    axios.get('http://10.0.0.164:3000/api/v1/local_games/3')
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 
   render() {
